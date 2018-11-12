9ea423b6a79ee2ef838615373dcb47a79286491a
黄扬 <39240132+playbat@users.noreply.github.com>
2018-11-12 18:46:38 +0800

    Create Multi_dir_rmdir_depth.sh

---
+++ Multi_dir_rmdir_depth.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Building environment, please waiting..."
+#sleep 2
+echo "Step 1, working..."
+mkdir -p test/123/345/789
+#sleep 2
+echo "Done!"
+#sleep 2
+echo "Step 2, working..."
+mkdir -p test/abc/def/ghi
+#sleep 2
+echo "Done!"
+#sleep 2
+echo "Step 3, working..."
+mkdir -p test/louis/Desktop/aaa
+#sleep 2
+echo "Done!"
+#sleep 2
+echo "Step 4, working..."
+mkdir -p test/louis/Documents/aaa
+#sleep 2
+echo "Done!"
+#sleep 2
+echo "Step 5, working..."
+mkdir -p test/louis/Downloads/aaa
+#sleep 2
+echo "Done!"
+#sleep 2
+echo "Step 6, working..."
+mkdir -p test/louis/Videos/aaa
+#sleep 2
+echo "All done :)"
